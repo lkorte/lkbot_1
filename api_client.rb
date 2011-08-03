2c411612d15a9480f626c5ebb5bcd7fd2671227d
Lutz Korte <lutz@eludos.de>
2011-08-04 00:24:57 +0200

    added api call, removed some debug messages

--- api_client.rb
+++ api_client.rb
@@ -2,7 +2,7 @@ require 'httparty'
 
 class ApiClient
   include HTTParty
-  base_uri 'eludos.de'
+  base_uri 'botliga.de'
 
   def self.post_to_api match_id, result, bot_token
     post('/api/guess', :query => {:match_id => match_id, :result => result, :token => bot_token})
--- app.rb
+++ app.rb
@@ -5,22 +5,21 @@ require 'yaml'
 require 'mongoid'
 require 'match'
 require 'guess'
+require 'api_client'
 
 Mongoid.database = Mongo::Connection.new('localhost','27017').db('botliga')
 
 token_list = YAML.load_file('tokens.yml')
-puts token_list.inspect
 bot_token = token_list['bot1']
 
-(1..1).each do |group|
+(1..34).each do |group|
 	matches = Match.where(:group => group)
 	
 	matches.each do |match|
 		guess = Guess.new
 		result = guess.result
-		puts "#{match.matchId} #{match.hostName} - #{match.guestName}, #{match.hostGoals}:#{match.guestGoals}"
-		puts "#{match.matchId}, #{result}, #{bot_token}"
-		#puts ApiClient.post_to_api(match.matchId, result, bot_token)
+		puts "#{match.matchId} #{match.hostName} - #{match.guestName}, #{match.hostGoals}:#{match.guestGoals}, guess: #{result}"
+		ApiClient.post_to_api(match.matchId, result, bot_token)
 	end
 	puts 
 	puts "----------------------------------------------------------------------"
